4961a5cd7eccb8789c82aaceae2752f4f028d6f0
mjturi <56698233+mjturi@users.noreply.github.com>
2020-11-29 17:38:27 -0800

    Update operations.php

--- operations.php
+++ operations.php
@@ -1,63 +1,77 @@
 <?php
-$deleteRow = "DELETE FROM %s WHERE id=%d";
-$getRow = "SELECT * FROM %s";
+
 
 function connectDatabase() {
-  $servername = "localhost";
-  $username = "system";
-  $password = "password";
-  $dbname = "myDB";
+    $servername = "localhost";
+    $username = "root";
+    $password = "*****";
 
-  $conn = new mysqli($servername, $username, $password, $dbname);
 
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-  return $conn;
+    $conn = new mysqli($servername, $username, $password);
+    $conn->select_db("website");
+
+    if ($conn->connect_error){
+        die("connection failed: " . $conn->connect_error);
+    }
+    return $conn;
 
 }
 
 function deleteData($table, $id) {
-  $conn = connectDatabase();
+    $conn = connectDatabase();
+    $deleteRow = "DELETE FROM %s WHERE id=%d";
 
-  if ($table == "Clubs") {
-    $sql = sprintf($deleteRow, $table, $id);
-  } elseif ($table == "Leagues") {
-    $sql = sprintf($deleteRow, $table, $id);
-  } elseif ($table == "Matches") {
-    $sql = sprintf($deleteRow, $table, $id);
-  } elseif ($table == "Players") {
-    $sql = sprintf($deleteRow, $table, $id);
-  }
+    if ($table == "Clubs") {
+        $sql = sprintf($deleteRow, $table, $id);
+    } elseif ($table == "Leagues") {
+        $sql = sprintf($deleteRow, $table, $id);
+    } elseif ($table == "Matches") {
+        $sql = sprintf($deleteRow, $table, $id);
+    } elseif ($table == "Players") {
+        $sql = sprintf($deleteRow, $table, $id);
+    }
 
-  if ($conn->query($sql) === TRUE) {
-    echo "Record deleted successfully";
-  } else {
-    echo "Error deleting record: " . $conn->error;
-  }
+    if ($conn->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
 
-  $conn->close();
+    $conn->close();
+}
+
+function getClub($id) {
+    $conn = connectDatabase();
+    $query = "SELECT name FROM clubs WHERE clubs.clubid = $id";
+    $result = $conn->query($query);
+    $name = $result->fetch_array()[0];
+    return $name;
 }
 
 function getData($table) {
-  $conn = connectDatabase();
+    $conn = connectDatabase();
+    $getRow = "SELECT * FROM %s";
 
-  if ($table == "Clubs") {
-    $sql = sprintf($getRow, $table);
-  } elseif ($table == "Leagues") {
-    $sql = sprintf($getRow, $table);
-  } elseif ($table == "Matches") {
-    $sql = sprintf($getRow, $table);
-  } elseif ($table == "Players") {
-    $sql = sprintf($getRow, $table);
-  }
+    $sql = "";
 
-  $result = $conn->query($sql);
+    if ($table == "clubs") {
+        $sql = sprintf($getRow, $table);
+    } elseif ($table == "leagues") {
+        $sql = sprintf($getRow, $table);
+    } elseif ($table == "matches") {
+        $sql = sprintf($getRow, $table);
+    } elseif ($table == "players") {
+        $sql = sprintf($getRow, $table);
+    }
 
-  $result -> fetch_all(MYSQLI_ASSOC);
+    $result = $conn->query($sql);
+    $json = [];
+    while ($row = $result->fetch_assoc()){
+        $json[] = $row;
+    }
 
-
-  $conn->close();
-  return $result;
+    $conn->close();
+    return $result;
 }
+
 ?>
