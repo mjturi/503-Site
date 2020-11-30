3acf6edc9da4ac5b0d50069231da68a84ab7077d
mjturi <56698233+mjturi@users.noreply.github.com>
2020-11-29 17:38:02 -0800

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,9 @@ session_start();
 	<title>Soccer Leagues</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-		<?php include 'operations.php';?>
+    <link rel="stylesheet" href="./css/index.css">
+		<?php include 'operations.php'; ?>
+
 </head>
 
 
@@ -17,7 +18,7 @@ session_start();
 	<header>
 		<div id="logo">
 			<a href="https://www.sdsu.edu/">
-			<img src="football.svg">
+			<img src="./images/football.svg">
 			</a>
 			<a href="index.php">
 				<h1>Soccer Leagues</h1>
@@ -38,20 +39,20 @@ session_start();
  <div>
   <h1>Leagues</h1>
 	<a class="padding_left_20" href="https://www.sdsu.edu/">
-		<img src="add.svg" width="30px" height="30px" alt="Add Club">
+		<img src="./images/add.svg" width="30px" height="30px" alt="Add Club">
 	</a>
   <div id="leagues" class="columns">
     <?php
-		$leagues = getData('Leagues');
+		$leagues = getData('leagues');
 		foreach($leagues as $league): ?>
-		?>
+
       <article class="column">
 				<span id="actions">
 					<a href="#">
-	          <img src="pencil.svg" width="20px" height="20px" alt="Edit"/>
+	          <img src="./images/pencil.svg" width="20px" height="20px" alt="Edit"/>
 	        </a>
 					<a href="#" class="padding_left_20">
-	          <img src="trash-can.svg" width="20px" height="20px" alt="Delete"/>
+	          <img src="./images/trash-can.svg" width="20px" height="20px" alt="Delete"/>
 	        </a>
 				</span>
         <h2 class="clear_both"><?php echo $league['name']; ?></h2>
@@ -65,7 +66,7 @@ session_start();
  <div id="clubs">
   <h1 class="padding_bottom_40">Clubs</h1>
 	<a class="padding_left_20" href="https://www.sdsu.edu/">
-		<img src="add.svg" width="30px" height="30px" alt="Add Club">
+		<img src="./images/add.svg" width="30px" height="30px" alt="Add Club">
 	</a>
 	<table>
   <tr>
@@ -75,24 +76,27 @@ session_start();
     <th>Loss</th>
 		<th>Draw</th>
 		<th>Points</th>
-		<th>Action</th>
+		<th>Edit</th>
+        <th>Delete</th>
   </tr>
 	<?php
-	$clubs = getData('Clubs');
+	$clubs = getData('clubs');
 	foreach($clubs as $club): ?>
   <tr>
     <td><?php echo $club['ranking']; ?></td>
-    <td><?php echo $club['club']; ?></td>
+    <td><?php echo $club['name']; ?></td>
     <td><?php echo $club['wins']; ?></td>
-		<td><?php echo $club['loss']; ?>]</td>
-    <td><?php echo $club['draw']; ?></td>
+		<td><?php echo $club['losses']; ?></td>
+    <td><?php echo $club['draws']; ?></td>
     <td><?php echo $club['points']; ?></td>
 		<td>
-			<a href="#">
-				<img src="pencil.svg" width="20px" height="20px" alt="Edit"/>
-			</a>
+            <a href="#">
+                <img src="./images/pencil.svg" width="20px" height="20px" alt="Edit"/>
+            </a>
+        </td>
+        <td>
 			<a href="#" class="padding_left_20">
-				<img src="trash-can.svg" width="20px" height="20px" alt="Delete"/>
+				<img src="./images/trash-can.svg" width="20px" height="20px" alt="Delete"/>
 			</a>
 		</td>
   </tr>
@@ -104,26 +108,31 @@ session_start();
  <div id="matches">
   <h1>Matches</h1>
 	<a class="padding_left_20" href="https://www.sdsu.edu/">
-		<img src="add.svg" width="30px" height="30px" alt="Add Match">
+		<img src="./images/add.svg" width="30px" height="30px" alt="Add Match">
 	</a>
 	<div class="scrollmenu">
     <?php
-		$matches = getData('Matches');
+		$matches = getData('matches');
+		$clubs = getData('clubs');
 		foreach($matches as $match): ?>
       <article class="column">
 				<span id="actions">
 					<a href="#">
-	          <img src="pencil.svg" width="20px" height="20px" alt="Edit"/>
+	          <img src="./images/pencil.svg" width="20px" height="20px" alt="Edit"/>
 	        </a>
 					<a href="#" class="padding_left_20">
-	          <img src="trash-can.svg" width="20px" height="20px" alt="Delete"/>
+	          <img src="./images/trash-can.svg" width="20px" height="20px" alt="Delete"/>
 	        </a>
 				</span>
 			</br>
 				<div class = "padding_bottom_20">
-					<?php echo $match['']; ?> &nbsp;
-					<span class="text-color"><?php echo $match['']; ?> - <?php echo $match['']; ?></span>
-					&nbsp; <?php echo $match['']; ?>
+					<?php
+                    $home = getClub($match['homeclubid']);
+                    $vis = getClub($match['visitorclubid']);
+
+                    echo $home; ?> vs <?php echo $vis; ?> &nbsp;
+                    <br>
+					<span class="text-color"><?php echo $match['homescore']; ?> - <?php echo $match['visitorscore']; ?></span>
 				</div>
       </article>
     <?php endforeach; ?>
@@ -133,24 +142,24 @@ session_start();
  <div id="players">
   <h1>Players</h1>
 	<a class="padding_left_20" href="https://www.sdsu.edu/">
-		<img src="add.svg" width="30px" height="30px" alt="Add Match">
+		<img src="./images/add.svg" width="30px" height="30px" alt="Add Match">
 	</a>
 	<div class="scrollmenu">
     <?php
-		$players = getData('Players');
+		$players = getData('players');
 		foreach($players as $player): ?>
       <article class="column">
 				<span id="actions">
 					<a href="#">
-	          <img src="pencil.svg" width="20px" height="20px" alt="Edit"/>
+	          <img src="./images/pencil.svg" width="20px" height="20px" alt="Edit"/>
 	        </a>
 					<a href="#" class="padding_left_20">
-	          <img src="trash-can.svg" width="20px" height="20px" alt="Delete"/>
+	          <img src="./images/trash-can.svg" width="20px" height="20px" alt="Delete"/>
 	        </a>
 				</span>
-				<div class="numberCircle padding_bottom_20 clear_both">90</div>
-				<div>PW</div>
-        <h3 class="margin_top_12"><?php echo "player name"; ?></h3>
+				<div class="numberCircle padding_bottom_20 clear_both"><?php echo $player['rating'];?></div>
+				<div><?php echo $player['position'];?></div>
+        <h3 class="margin_top_12"><?php echo $player['name']; ?></h3>
 				<table>
 			  <tr>
 			    <td>Age:</td>
@@ -178,7 +187,7 @@ session_start();
 				</tr>
 				<tr>
 					<td>Market Value:</td>
-			    <td><?php echo $player['market_value'];?></td>
+			    <td><?php echo $player['marketval'];?></td>
 				</tr>
 			 </table>
       </article>
