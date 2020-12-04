80e95cbe5ea4c70771b977c4c3975eb77b5f614b
mjturi <56698233+mjturi@users.noreply.github.com>
2020-12-04 15:45:29 -0800

    Add files via upload

---
+++ update.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/update.css" media="screen">
+</head>
+<?php include 'header.php'; ?>
+<body>
+<br>
+    <h1>Record Updated Successfully!</h1>
+</body>
+</html>
---
+++ update2.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/update.css" media="screen">
+    </head>
+    <?php include 'header.php'; ?>
+    <body>
+    <br>
+    <h1>Error updating record. Please try again.</h1>
+    </body>
+</html>
